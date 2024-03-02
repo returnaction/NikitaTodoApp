fd7ee04340b106a4b7a168b5d99030c0aad4214b
returnaction <obergannv@gmail.com>
2024-03-01 22:12:53 -0500

    Add ITodoRepository and Implemeted TodoRepository

---
+++ NikitaTodoApp/Repository/IRepository/ITodoRepository.cs
@@ -0,0 +1,9 @@
+﻿using NikitaTodoApp.Models;
+
+namespace NikitaTodoApp.Repository.IRepository
+{
+    public interface ITodoRepository : IRepository<Todo>
+    {
+        void Update(Todo todo);
+    }
+}
---
+++ NikitaTodoApp/Repository/TodoRepository.cs
@@ -0,0 +1,20 @@
+﻿using NikitaTodoApp.Data;
+using NikitaTodoApp.Models;
+using NikitaTodoApp.Repository.IRepository;
+
+namespace NikitaTodoApp.Repository
+{
+    public class TodoRepository : Repository<Todo>, ITodoRepository
+    {
+        private ApplicationDbContext _db;
+        public TodoRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Todo todo)
+        {
+            _db.Todos.Update(todo);
+        }
+    }
+}
