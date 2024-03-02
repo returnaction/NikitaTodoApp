385846fbca64fa053fa75deccecf2647b8721f7c
returnaction <obergannv@gmail.com>
2024-03-01 21:35:50 -0500

    Add Todo Model

---
+++ NikitaTodoApp/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NikitaTodoApp.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Title { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
