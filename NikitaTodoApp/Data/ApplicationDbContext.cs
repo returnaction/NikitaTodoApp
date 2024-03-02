a74d51f18f6d804d1c2b952428898d24ddb6f67d
returnaction <obergannv@gmail.com>
2024-03-01 21:47:16 -0500

    Fixed ApplicationDbContext class

--- NikitaTodoApp/Data/ApplicationDbContext.cs
+++ NikitaTodoApp/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace NikitaTodoApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        protected ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
--- NikitaTodoApp/Program.cs
+++ NikitaTodoApp/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
