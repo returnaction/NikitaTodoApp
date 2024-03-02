2073ca551a4ced4e068bca1d88adf064a91d3d7e
returnaction <obergannv@gmail.com>
2024-03-01 23:19:07 -0500

    Add Upset View

--- NikitaTodoApp/Controllers/HomeController.cs
+++ NikitaTodoApp/Controllers/HomeController.cs
@@ -9,17 +9,33 @@ namespace NikitaTodoApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
             List<Todo>? todos = _unitOfWork.Todo.GetAll().ToList();
             return View(todos);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                Todo todo = new();
+                return View(todo);
+            }
+            else
+            {
+                Todo? todo = _unitOfWork.Todo.Get(todo => todo.Id == id);
+                return View(todo);
+            }
         }
 
         public IActionResult Privacy()
