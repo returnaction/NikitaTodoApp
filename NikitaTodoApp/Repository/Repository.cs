cecf21388117a8bcfaa4c1cf4f8171de285a2fb8
returnaction <obergannv@gmail.com>
2024-03-01 22:03:41 -0500

    Add Repository

---
+++ NikitaTodoApp/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NikitaTodoApp.Data;
+using NikitaTodoApp.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace NikitaTodoApp.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbset;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbset = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbset;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault()!;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbset;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbset.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbset.RemoveRange(entity);
+        }
+    }
+}
