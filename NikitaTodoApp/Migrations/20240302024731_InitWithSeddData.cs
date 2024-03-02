1be5be00c919fde62cfd7d63d2f9e10d5e517c39
returnaction <obergannv@gmail.com>
2024-03-01 21:58:13 -0500

    add IRepository

---
+++ NikitaTodoApp/Migrations/20240302024731_InitWithSeddData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NikitaTodoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitWithSeddData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Description", "IsCompleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Practise .net everyday 4 hours minimum", false, "Learn .NET" },
+                    { 2, "Wash dishes in the dishwasher", true, "Wash Dishes" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
